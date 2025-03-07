075e52422053b1db6ca0cbd77238176c72ce7bcf
mirohid <mirohid7860@gmail.com>
2025-03-07 12:55:12 +0530

    interview Questions added

---
+++ SwiftUI-Animations/Interview-Questions.swift
@@ -0,0 +1,44 @@
+//
+//  Questions.swift
+//  SwiftUI-Animations
+//
+//  Created by MacMini6 on 07/03/25.
+//
+
+
+/*
+Basic Questions:-
+ 1. What is SwiftUI, and how does it differ from UIKit ?
+ 2 What are the property wrappers in SwiftUI ?
+ 3. What is the diffrence between @state and @Binding ?
+ 4. How does SwiftUI Handle data flow ?
+ 5. What i sthe diffence between @ObservedObject and @EnviromentObject ?
+ 6. What is the role of @StateObject ?
+ 7. How does SwiftUI manage UI updates efficently?
+ 
+ 
+ Advancd Questions:-
+  8. What is @AppStorage, and when should you use it?
+  9. How do you create a custom SwiftUI modifier ?
+  10. How do you handle Navigation in SwiftUI ?
+  11. How do you optimise performance in SwiftUI ?
+  12. What are the key difference between @State, @StateObject and @ObservedObject ?
+  13. How can you use Combine with SwiftUI ?
+  14. How does SwiftUI handle animations ?
+  15. What is a PrefernceKey, and when would you use it ?
+ 
+ Expert-Level Questions:-
+  16. How does SWiftUI handle dependency injections?
+  17. How do you implement a complex custom view in SWiftUI ?
+  18. How can you manage themes (LIght/ Dark Mode) dynamically in SwiftUI ?
+  19. What are the limitations of SwiftUI compared in UIKit ?
+  20 . How do you debug swiftUI views effectively ?
+  21. How does SwiftUI handle accessibility ?
+  22. How do you build a dynamic list using ForEach ?
+  23. What is the role of GeometryReader in SwiftUI ?
+  24. How can you integrate UIKit views into SwiftUI ?
+  25. What are LazyVStack and LazyHStack, and how do they improve performance ?
+ 
+ 
+ 
+*/
